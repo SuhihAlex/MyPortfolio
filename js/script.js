2bf422ac2e086d935ed30075eb6cdb4e4b615ca0
Alex <suhari93@mail.ru>
2024-04-27 00:14:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,125 @@
+/* Progress Bar */
+
+function progressBar() {
+  let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+  let height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  let scrolled = (scroll / height) * 100;
+
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+window.addEventListener("scroll", progressBar);
+
+window.addEventListener("scroll", function() {
+    const aboutBlock = document.querySelector('.about__block');
+    const aboutPosition = aboutBlock.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (aboutPosition < windowHeight / 1.5) {
+        aboutBlock.classList.add('show');
+    } else {
+        aboutBlock.classList.remove('show');
+    }
+});
+
+window.addEventListener("scroll", function() {
+    const projectsBlock = document.querySelector('.projects__block');
+    const projectsPosition = projectsBlock.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (projectsPosition < windowHeight / 1.5 + scrollPosition) {
+        projectsBlock.classList.add('show');
+    } else {
+        projectsBlock.classList.remove('show');
+    }
+});
+
+window.addEventListener("scroll", function() {
+    const portfolioBlock = document.querySelector('.projects__card-portfolio');
+    const trinityBlock = document.querySelector('.projects__card-trinity');
+    const rivoBlock = document.querySelector('.projects__card-rivo');
+    const portfolioPosition = portfolioBlock.getBoundingClientRect().top;
+    const trinityPosition = trinityBlock.getBoundingClientRect().top;
+    const rivoPosition = rivoBlock.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (portfolioPosition < windowHeight / 1.5) {
+        portfolioBlock.classList.add('show');
+    } else {
+        portfolioBlock.classList.remove('show');
+    }
+
+    if (trinityPosition < windowHeight / 1.5) {
+        trinityBlock.classList.add('show');
+    } else {
+        trinityBlock.classList.remove('show');
+    }
+
+    if (rivoPosition < windowHeight / 1.5) {
+        rivoBlock.classList.add('show');
+    } else {
+        rivoBlock.classList.remove('show');
+    }
+});
+
+
+// Плавное появление блоков
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.block');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+/*====== BURGER ======*/
+function burgerMenu() {
+  const menuToggle = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+
+  menu.classList.toggle('active');
+
+  if (menu.classList.contains('active')) {
+    menuToggle.classList.add('active');
+    document.body.style.overflow = 'hidden'; // Запрет прокрутки страницы при открытом меню
+  } else {
+    menuToggle.classList.remove('active');
+    document.body.style.overflow = ''; // Разрешение прокрутки страницы при закрытом меню
+  }
+}
+
+document.querySelector('.burger').addEventListener('click', function() {
+  this.classList.toggle('open');
+});
+
+/* SmoothScroll */
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    let href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    const topOffset = document.querySelector('.scrollto').offsetHeight;
+
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
