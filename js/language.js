12054e8e3eea913fe3f44dff931e91903867d02d
Alexandr Suhih <suhari93@mail.ru>
2024-05-04 01:17:10 +0300

    language.js

--- js/language.js
+++ js/language.js
@@ -9,8 +9,39 @@ function loadLanguageData() {
             "news": "News",
             "blog": "Blog",
             "contact": "Contact",
+            "heroBackgroundTitleLeft": "UI/UX Designer",
+            "heroBackgroundTitleRight": "Frontend Developer",
+            "heroDescription": "Hey There I’m",
+            "heroTitle": "Alex",
+            "heroSubtitle": "I'm currently learning FrontEnd development",
+            "aboutSection": "About me",
             "aboutTitle": "About me",
-            // Другие переводы для английского
+            "aboutDescription": "Hi, I'm Alexander, passionate about creating user-centered solutions. I specialize in UI/UX design, frontend development and graphic design, I am proficient in HTML, CSS, JavaScript, Node.js, jQuery, Bootstrap and Figma. Aside from coding, I love playing video games, graphic design, and listening to music. Let's chat and bring your digital ideas to life",
+            "aboutLink": "More...",
+            "projectsSection": "Recent Projects",
+            "seeAll": "See all",
+            "portfolioTitle": "Portfolio",
+            "portfolioTechnology": "Frontend",
+            "portfolioDescription": "Developed to showcase my skills in web development, my portfolio website exemplifies proficiency in technologies such as HTML, CSS, JS and Figma.",
+            "trinityTitle": "Trinity",
+            "trinityTechnology": "Frontend",
+            "trinityDescription": "Created to showcase my web development skills, the car hire dubai website demonstrates mastery of technologies such as HTML, CSS, JS, PHP and Figma.",
+            "rivoTitle": "Rivo",
+            "rivoTechnology": "Frontend",
+            "rivoDescription": "Rivo website is a multi-page website with many complex elements, this project used HTML, CSS, JS, jQuery, PHP and built using Gulp",
+            "contactsTitle": "Get in Touch",
+            "contactsSubtitle": "If you are interested in my work or want to provide feedback about this website, I am open to exchanging ideas.",
+            "nameLabel": "Name",
+            "emailLabel": "Email",
+            "messageLabel": "Message",
+            "sendButton": "Send",
+            "footerTitle": "Alex.",
+            "footerSubtitle": "I am a web developer living in Moldova. I have a very deep passion for Front End Development ❤ This site is made with a lot of love and passion.",
+            "portfolioVersion": "Portfolio v2.0",
+            "copyright": "© 2024 Alex",
+            "about_text": "I'm Alex, FrontEnd Development. My passion lies in creating user-centred experiences as a UI/UX designer and frontend developer. With a keen attention to detail and a love for clean code, I specialise in building high-performance websites and applications. My experience spans HTML, CSS, JavaScript, Node.js, Gulp, Sass, jQuery, and Bootstrap. From concept to implementation, I strive to create digital experiences that seamlessly blend functionality and aesthetics. My journey is not limited to just coding. Collaboration is at the heart of my approach, as I believe the best results are achieved through teamwork and communication. Outside the realm of technology, I find solace in creative pursuits. Graphic design brings out my creativity in the digital realm. And when I need to recharge, you'll often find me playing video games. So whether you're looking for an experienced developer, creative designer or team player, I'm ready to bring your digital ideas to life. Let's embark on an exciting innovation journey together!",
+            "download_resume": "Download Resume",
+            "skills": "Skills",
         },
         "ru": {
             "home": "Главная",
@@ -19,8 +50,39 @@ function loadLanguageData() {
             "news": "Новости",
             "blog": "Блог",
             "contact": "Контакты",
+            "heroBackgroundTitleLeft": "UI/UX Дизайнер",
+            "heroBackgroundTitleRight": "Фронтенд разработчик",
+            "heroDescription": "Привет, я",
+            "heroTitle": "Александр",
+            "heroSubtitle": "В настоящее время изучаю разработку веб-интерфейсов",
+            "aboutSection": "Обо мне",
             "aboutTitle": "Обо мне",
-            // Другие переводы для русского
+            "aboutDescription": "Привет, я Александр, увлеченный созданием решений, ориентированных на пользователя. Я специализируюсь на дизайне интерфейсов и пользовательских исследованиях, фронтенд-разработке и графическом дизайне, владею HTML, CSS, JavaScript, Node.js, jQuery, Bootstrap и Figma. Помимо программирования, я люблю играть в видеоигры, заниматься графическим дизайном и слушать музыку. Давайте поболтаем и воплотим ваши цифровые идеи в жизнь",
+            "aboutLink": "Подробнее...",
+            "projectsSection": "Недавние проекты",
+            "seeAll": "Посмотреть все",
+            "portfolioTitle": "Portfolio",
+            "portfolioTechnology": "Фронтенд",
+            "portfolioDescription": "Разработанный для демонстрации моих навыков в веб-разработке, мой сайт-портфолио отличается мастерством в технологиях, таких как HTML, CSS, JS и Figma.",
+            "trinityTitle": "Trinity",
+            "trinityTechnology": "Фронтенд",
+            "trinityDescription": "Созданный для демонстрации моих навыков веб-разработки, сайт аренды автомобилей в Дубае демонстрирует владение технологиями, такими как HTML, CSS, JS, PHP и Figma.",
+            "rivoTitle": "Rivo",
+            "rivoTechnology": "Фронтенд",
+            "rivoDescription": "Веб-сайт Rivo - это многостраничный сайт с множеством сложных элементов, в этом проекте использовались HTML, CSS, JS, jQuery, PHP и сборка с использованием Gulp",
+            "contactsTitle": "Свяжитесь со мной",
+            "contactsSubtitle": "Если вас заинтересовала моя работа или вы хотите оставить отзыв о этом сайте, я открыт для обмена идеями.",
+            "nameLabel": "Имя",
+            "emailLabel": "Эл. почта",
+            "messageLabel": "Сообщение",
+            "sendButton": "Отправить",
+            "footerTitle": "Алекс.",
+            "footerSubtitle": "Я веб-разработчик, живущий в Молдове. У меня очень глубокая страсть к Front End Development ❤ Этот сайт сделан с большой любовью и страстью.",
+            "portfolioVersion": "Портфолио v2.0",
+            "copyright": "© 2024 Алекс",
+            "about_text": "Я Алекс, разработчик фронтенда. Моя страсть - создавать пользовательские интерфейсы в качестве дизайнера и фронтенд разработчика. Обладая вниманием к деталям и любовью к чистому коду, я специализируюсь на создании высокопроизводительных веб-сайтов и приложений. Мой опыт включает HTML, CSS, JavaScript, Node.js, Gulp, Sass, jQuery и Bootstrap. От концепции до воплощения, я стремлюсь создавать цифровые интерфейсы, которые безупречно сочетают в себе функциональность и эстетику. Мой путь не ограничивается только программированием. Сотрудничество - вот главное в моем подходе, так как я верю, что лучшие результаты достигаются через командную работу и общение. Вне мира технологий я нахожу утешение в творчестве. Графический дизайн раскрывает мою креативность в цифровом мире. И когда мне нужно подзарядиться, вы часто найдете меня, играющим в видеоигры. Так что, будь то опытный разработчик, креативный дизайнер или командный игрок, я готов воплотить ваши цифровые идеи в жизнь. Давайте начнем увлекательное путешествие вместе!",
+            "download_resume": "Скачать резюме",
+            "skills": "Навыки"
         },
         "ro": {
             "home": "Acasă",
@@ -29,8 +91,39 @@ function loadLanguageData() {
             "news": "Noutăți",
             "blog": "Blog",
             "contact": "Contact",
+            "heroBackgroundTitleLeft": "UI/UX Designer",
+            "heroBackgroundTitleRight": "Frontend Developer",
+            "heroDescription": "Bună, eu sunt",
+            "heroTitle": "Alex",
+            "heroSubtitle": "În prezent învăț dezvoltare frontend",
+            "aboutSection": "Despre mine",
             "aboutTitle": "Despre mine",
-            // Другие переводы для румынского
+            "aboutDescription": "Salut, sunt Alexandru, pasionat de crearea de soluții centrate pe utilizator. Mă specializez în design UI/UX, dezvoltare frontend și design grafic, sunt proficient în HTML, CSS, JavaScript, Node.js, jQuery, Bootstrap și Figma. Pe lângă programare, îmi place să joc jocuri video, să fac design grafic și să ascult muzică. Hai să discutăm și să aducem la viață ideile tale digitale",
+            "aboutLink": "Mai mult...",
+            "projectsSection": "Proiecte recente",
+            "seeAll": "Vezi tot",
+            "portfolioTitle": "Portfolio",
+            "portfolioTechnology": "Frontend",
+            "portfolioDescription": "Dezvoltat pentru a-mi prezenta abilitățile în dezvoltarea web, site-ul meu de portofoliu exemplifică competența în tehnologii precum HTML, CSS, JS și Figma.",
+            "trinityTitle": "Trinity",
+            "trinityTechnology": "Frontend",
+            "trinityDescription": "Creat pentru a-mi prezenta abilitățile în dezvoltarea web, site-ul de închiriere auto din Dubai demonstrează stăpânirea tehnologiilor precum HTML, CSS, JS, PHP și Figma.",
+            "rivoTitle": "Rivo",
+            "rivoTechnology": "Frontend",
+            "rivoDescription": "Site-ul Rivo este un site multi-pagină cu multe elemente complexe, acest proiect folosește HTML, CSS, JS, jQuery, PHP și este construit folosind Gulp",
+            "contactsTitle": "Ia legătura",
+            "contactsSubtitle": "Dacă sunteți interesat de munca mea sau doriți să oferiți feedback despre acest site, sunt deschis să schimb idei.",
+            "nameLabel": "Nume",
+            "emailLabel": "Email",
+            "messageLabel": "Mesaj",
+            "sendButton": "Trimite",
+            "footerTitle": "Alex.",
+            "footerSubtitle": "Sunt un dezvoltator web care locuiește în Moldova. Am o pasiune foarte profundă pentru dezvoltarea Front End ❤ Acest site este realizat cu multă dragoste și pasiune.",
+            "portfolioVersion": "Portofoliu v2.0",
+            "copyright": "© 2024 Alex",
+            "about_text": "Sunt Alex, dezvoltator FrontEnd. Pasiunea mea constă în crearea experiențelor centrate pe utilizator ca designer UI/UX și dezvoltator frontend. Cu o atenție deosebită la detalii și o dragoste pentru codul curat, mă specializez în construirea de site-uri web și aplicații de performanță înaltă. Experiența mea cuprinde HTML, CSS, JavaScript, Node.js, Gulp, Sass, jQuery și Bootstrap. De la concept la implementare, mă străduiesc să creez experiențe digitale care îmbină perfect funcționalitatea și estetica. Călătoria mea nu se limitează doar la codare. Colaborarea este la inima abordării mele, deoarece cred că cele mai bune rezultate se obțin prin muncă în echipă și comunicare. În afara domeniului tehnologiei, găsesc alinare în activitățile creative. Designul grafic scoate la iveală creativitatea mea în lumea digitală. Și atunci când trebuie să mă reîncarc, adesea mă găsești jucând jocuri video. Deci, fie că căutați un dezvoltator experimentat, un designer creativ sau un jucător de echipă, sunt gata să aduc ideile dvs. digitale la viață. Haideți să pornim împreună într-o călătorie palpitantă de inovație!",
+            "download_resume": "Descarcă CV",
+            "skills": "Abilități"
         }
     };
 }
@@ -47,7 +140,6 @@ function setLanguageOnPage(langData, lang) {
     });
 }
 
-// Функция для изменения языка
 function changeLanguage() {
     let select = document.getElementById("langSelect");
     let lang = select.options[select.selectedIndex].value;
@@ -60,15 +152,42 @@ function changeLanguage() {
 
     // Устанавливаем язык на странице
     setLanguageOnPage(langData, lang);
+
+    // Меняем стилизацию объектов в зависимости от выбранного языка,
+    // если элементы с соответствующими идентификаторами существуют на странице
+    if (lang === "ru") {
+        let heroTitle = document.getElementById("heroTitle");
+        let heroBackgroundBlock = document.getElementById("heroBackgroundBlock");
+        let projectsTitle = document.getElementById("projectsTitle");
+        let contactsTitle = document.getElementById("contactsTitle");
+        
+        if (heroTitle) heroTitle.classList.add("russian");
+        if (heroBackgroundBlock) heroBackgroundBlock.classList.add("russian");
+        if (projectsTitle) projectsTitle.classList.add("russian");
+        if (contactsTitle) contactsTitle.classList.add("russian");
+    } else {
+        let heroTitle = document.getElementById("heroTitle");
+        let heroBackgroundBlock = document.getElementById("heroBackgroundBlock");
+        let projectsTitle = document.getElementById("projectsTitle");
+        let contactsTitle = document.getElementById("contactsTitle");
+        
+        if (heroTitle) heroTitle.classList.remove("russian");
+        if (heroBackgroundBlock) heroBackgroundBlock.classList.remove("russian");
+        if (projectsTitle) projectsTitle.classList.remove("russian");
+        if (contactsTitle) contactsTitle.classList.remove("russian");
+    }
 }
 
-// Вызываем функцию установки выбранного языка после загрузки страницы
 document.addEventListener("DOMContentLoaded", function() {
-    let lang = localStorage.getItem("selectedLanguage") || "en";
-    let langData = loadLanguageData();
-    setLanguageOnPage(langData, lang);
+    let lang = localStorage.getItem("selectedLanguage") || "en"; // Если язык не установлен в localStorage, используем en по умолчанию
+    let select = document.getElementById("langSelect");
+    select.value = lang; // Устанавливаем сохраненное значение языка в селекте
+
+    // Вызываем функцию изменения языка, чтобы применить класс russian сразу после загрузки страницы
+    changeLanguage();
 });
 
-// Связываем событие изменения языка с функцией changeLanguage
 document.getElementById("langSelect").addEventListener("change", changeLanguage);
+
+
 
